8696979e3ef04b831a2e91b94a1fa8231839111e
KevinKinnunen <54663592+KevinKinnunen@users.noreply.github.com>
2023-10-26 11:11:50 +0200

    sync-test

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-System.Console.WriteLine("test");
-System.Console.WriteLine("test från kevin-branch");
-System.Console.WriteLine("test2222");
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
