cde7829b9c5d07608ccb0316e71ac945b23b6e0d
KevinKinnunen <54663592+KevinKinnunen@users.noreply.github.com>
2023-10-26 10:12:35 +0200

    test
    från kevin-branch

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 System.Console.WriteLine("test");
+System.Console.WriteLine("test från kevin-branch");
